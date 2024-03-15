6bcfc4aeefad4b7c1c3004a4ed567ecedb0b11e4
Jacob George <jacobgeorge5566@gmail.com>
2024-03-15 16:44:06 +0530

    asas

--- src/compoent/navbar/Navbar.jsx
+++ src/compoent/navbar/Navbar.jsx
@@ -1,7 +1,4 @@
-import Link from "next/link"
-import Links from "./links/LInks"
-
-
+import Links from "./links/Links";
 const Navbar = () => {
   return (
     <div className="flex justify-between w-[100%] mt-6">
